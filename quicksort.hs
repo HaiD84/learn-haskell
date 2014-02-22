3e77c7c36a2e293c62374f7b760c379674117f3f
Dmitry Khayrutdinov <haid84@gmail.com>
2014-02-22 15:11:03 +0400

    Recursion, quicksort

---
+++ quicksort.hs
@@ -0,0 +1,11 @@
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+    let smallerSorted = quicksort [ a | a <- xs, a <= x]
+        biggerSorted = quicksort [ a | a <- xs, a > x]
+    in smallerSorted ++ [x] ++ biggerSorted
+
+{- or in one line: -}
+quicksort' :: (Ord a) => [a] -> [a]
+quicksort' [] = []
+quicksort' (x:xs) = quicksort [ a | a <- xs, a <= x] ++ [x] ++ quicksort [ a | a <- xs, a > x]
