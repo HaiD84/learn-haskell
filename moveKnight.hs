e1acfb429ba41c5080dc7bb0dcc2181cdb7b3914
Dmitry Khayrutdinov <haid84@gmail.com>
2014-06-12 23:18:15 +0400

    Move knight in Monad way

---
+++ moveKnight.hs
@@ -0,0 +1,20 @@
+import Control.Monad
+
+
+type KnightPos = (Int, Int)
+
+moveKnight :: KnightPos -> [KnightPos]
+moveKnight (c, r) = do
+    (c', r') <- [
+        (c - 2, r - 1), (c - 2, r + 1),
+        (c - 1, r + 2), (c + 1, r + 2),
+        (c + 2, r + 1), (c + 2, r - 1),
+        (c - 1, r - 2), (c + 1, r - 2)]
+    guard (c' `elem` [1..8] && r' `elem` [1..8])
+    return (c', r')
+
+in3Moves :: KnightPos -> [KnightPos]
+in3Moves start = return start >>= moveKnight >>= moveKnight >>= moveKnight
+
+canReachIn3Moves :: KnightPos -> KnightPos -> Bool
+canReachIn3Moves start end = end `elem` in3Moves start
