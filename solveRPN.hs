490e03628a4547ec3e0e72137f76d91d1b06c25d
Dmitry Khayrutdinov <haid84@gmail.com>
2014-08-03 20:38:12 +0400

    Improve RPN solver to handle errors

--- solveRPN.hs
+++ solveRPN.hs
@@ -1,13 +1,23 @@
+import Control.Monad
+
 {-
  - Вычисление обратной польской записи:
  - *Main> solveRPN "2 2 2 * +"
  - 6.0
  -}
-solveRPN :: String -> Double
-solveRPN = head . foldl foldingFunction [] . words
-    where
-        foldingFunction (x:y:xs) "+" = (y + x) : xs
-        foldingFunction (x:y:xs) "-" = (y - x) : xs
-        foldingFunction (x:y:xs) "*" = (y * x) : xs
-        foldingFunction (x:y:xs) "/" = (y / x) : xs
-        foldingFunction xs numberString = read numberString : xs
+solveRPN :: String -> Maybe Double
+solveRPN str = do
+    [result] <- foldM foldingFunction [] (words str)
+    return result
+
+foldingFunction :: [Double] -> String -> Maybe [Double]
+foldingFunction (x:y:xs) "+" = return ((y + x) : xs)
+foldingFunction (x:y:xs) "-" = return ((y - x) : xs)
+foldingFunction (x:y:xs) "*" = return ((y * x) : xs)
+foldingFunction (x:y:xs) "/" = return ((y / x) : xs)
+foldingFunction xs numberString = liftM (:xs) (readMaybe numberString)
+
+readMaybe :: (Read a) => String -> Maybe a
+readMaybe s = case reads s of
+    [(x, "")] -> Just x
+    _ -> Nothing
